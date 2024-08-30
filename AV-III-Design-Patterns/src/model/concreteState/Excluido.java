559c1f952e79503daf59d80c2b2239a69901b632
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2024-08-30 19:03:40 -0300

    implementando ArquivoState (Excluido.java)

--- AV-III-Design-Patterns/src/model/concreteState/Excluido.java
+++ AV-III-Design-Patterns/src/model/concreteState/Excluido.java
@@ -1,10 +1,11 @@
 package model.concreteState;
 
 import model.state.AbstractArquivoState;
+import model.state.ArquivoState;
 
 import javax.naming.OperationNotSupportedException;
 
-public class Excluido extends AbstractArquivoState {
+public class Excluido extends AbstractArquivoState implements ArquivoState{
     @Override
     public Long getTamanho(String conteudo) throws IllegalAccessException {
         return 0L;
